aa67bea6d49ec92acedc326078545d7b091a77a0
Ian Jenkins <ian@jenkins-web.co.uk>
2010-11-08 22:28:14 +0000

    Adding inspired directory and skeleton php file

---
+++ inspired/inspired.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: inspired
+Plugin URI: http://jenkins-web.co.uk/inspired
+Description: Plugin to bring in various content from social sites that have inspired you
+Author: Ian Jenkins
+Version: 1.0
+Author URI: http://www.jenkins-web.co.uk
+*/
+?>
